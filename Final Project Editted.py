46abf18a3fa4042461aef4a76e1f9b8a7df6472f
antony <antonykwan9999@gmail.com>
2016-07-31 21:31:34 -0700

    changed moves for pokemon

--- Final Project Editted.py
+++ Final Project Editted.py
@@ -104,45 +104,45 @@ def fight(player, enemy):
                 move_selection = int(input("Please input a valid group"))
         if role == "water":
             print("Choose a move")
-            print("1.hydro pump")
-            print("2.water pulse")
-            print("3.water gun")
-            print("4.water spout")
+            print("1.tackle")
+            print("2.water gun")
+            print("3.bubble")
+            print("4.bit")
 
             move_selection = int(input("Please enter a valid number from 1 - 4"))
             if move_selection == 1:
-                player = Character("hydro pump", 0, 110)
+                player = Character("tackle", 0, 50)
                 valid_input = True
             if move_selection == 2:
-                player = Character("water pulse", 0, 60)
-                valid_input = True
-            if move_selection == 3:
                 player = Character("water gun", 0, 40)
                 valid_input = True
+            if move_selection == 3:
+                player = Character("bubble", 0, 40)
+                valid_input = True
             if move_selection == 4:
-                player = Character("water spout", 0, 150 )
+                player = Character("bite", 0, 60)
                 valid_input = True
             if valid_input == False:
                 move_selection = int(input("Please input a valid group"))
         if role == "grass":
             print("Choose a move")
-            print("1.solar beam")
+            print("1.tackle")
             print("2.vine whip")
-            print("3.leaf storm")
-            print("4.frenzy plant")
+            print("3.take down")
+            print("4.razor leaf")
 
             move_selection = int(input("Please enter a valid number from 1 - 4"))
             if move_selection == 1:
-                player = Character("solar beam", 0, 120)
+                player = Character("tackle", 0, 50)
                 valid_input = True
             if move_selection == 2:
                 player = Character("vine whip", 0, 45)
                 valid_input = True
             if move_selection == 3:
-                player = Character("leaf storm", 0, 130)
+                player = Character("take down", 0, 90)
                 valid_input = True
             if move_selection == 4:
-                player = Character("frenzy plant", 0, 150)
+                player = Character("razor leaf", 0, 55)
                 valid_input = True
             if valid_input == False:
                 move_selection = int(input("Please input a valid group"))
