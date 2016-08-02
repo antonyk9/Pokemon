20f99cb216c2493228eb013a99bcb0a3d6f12279
antony <antonykwan9999@gmail.com>
2016-08-01 22:57:41 -0700

    editted

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,14 +1,21 @@
 import random
+#guessingisfun
+#pokemonisfun
 
 answer = random.randint(0, 20)
+print("I am thinking of a number between 0 - 20. Try to find that number.")
 guess = int(input("what is your guess?"))
-(x) = "number of guess == answer"
+
+guess_count = 1
+
 
 while guess != answer:
     if guess > answer:
-        guess = int(input("what is your guess?"))
+        guess = int(input("too high...what's your new guess?"))
+        guess_count = guess_count + 1
     elif guess < answer:
-        guess = int(input("what is your guess?"))
+        guess = int(input("too low...what's your new guess?"))
+        guess_count = guess_count + 1
     elif guess == answer:
         print("Correct")
         break
@@ -16,4 +23,4 @@ while guess != answer:
         guess = int(int(input("What is your guess?")))
 
 
-print("You guessed the number. Good job! Come back next time!")
+print("You guessed the number", guess_count,"times. Good job! Come play again!")
