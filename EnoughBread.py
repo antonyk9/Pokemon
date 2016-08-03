887ae5d16dc6d73c577a2811e9c0801c5872d2cd
antony <antonykwan9999@gmail.com>
2016-08-03 14:17:32 -0700

    added substances to my sandwich

--- EnoughBread.py
+++ EnoughBread.py
@@ -1,23 +1,29 @@
 slices_bread = int(input("How many slices of bread?"))
 slices_cheese = int(input("How any slices of cheese?"))
 slices_ham = int(input("How many slices of ham?"))
+slices_tomatoes = int(input("How many slices of tomatoes?"))
 num_students = 8
 
 
 if slices_bread / 2 >= num_students:
     print("Enough bread!")
 else:
-    print("Not enough bread. Students going hungry")
+    print("Not enough bread.")
 
 if slices_cheese >= num_students:
     print("Enough cheese!")
 else:
-    print("Not enough cheese. What a sad day......")
+    print("Not enough cheese.")
 
 if slices_ham >= num_students:
     print("Enough ham!")
 else:
-    print("Not enough ham. Need more protein")
+    print("Not enough ham.")
+
+if slices_tomatoes >= num_students:
+    print("Enough tomatoes!")
+else:
+    print("Not enough tomatoes.")
 
 
 enough_bread = slices_bread /2 >= num_students
