d55c9ef2090dfd54e0aa0419ebc66b7bb49ab36f
antony <antonykwan9999@gmail.com>
2016-08-04 14:17:48 -0700

    new project

---
+++ Account.py
@@ -0,0 +1,23 @@
+import random
+class Account:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+    def withdraw(self, amount):
+        self.balance -= amount
+    def deposit(self, amount):
+        self.balance += amount
+    def empty(self, balance):
+
+
+
+accounts = []
+for i in range(0, 10):
+    name = "account" + str(i)
+    balance = random.randint(1000, 2000)
+    accounts.append(Account(name, balance))
+
+# deposit money
+for account in accounts:
+    account.deposit(random.randint(500, 1000))
+    print(account.balance)
